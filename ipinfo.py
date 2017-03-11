0bf8b752509ea8df03571905d6c6074109170f34
Aswin <aswinkarthik.rv@gmail.com>
2017-03-11 14:48:29 +0100

    x-forwarded-for is added to return

--- ipinfo.py
+++ ipinfo.py
@@ -3,12 +3,12 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return str(request.remote_addr)
+    return str(request.headers.getlist("X-Forwarded-For"))
 
 @app.route("/json", methods=["GET"])
 def get_my_ip():
-    print (request.headers.getlist("X-Forwarded-For"))
-    return jsonify({'ip': request.remote_addr}), 200
+    
+    return jsonify({'ip': request.headers.getlist("X-Forwarded-For")}), 200
 
 if __name__ == "__main__":
     app.run(port=25500)
